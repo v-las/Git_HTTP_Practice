e4836cacd2a100e896cfdc32d1cf5031ab251ca1
Vlas_Mastykash <mastykash.vlas@ya.ru>
2021-09-07 11:28:35 +0600

    minor fixes

--- HW_01/myscript/myscript.sh
+++ HW_01/myscript/myscript.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-pwd
-mkdir foldername
-cd foldername
+mkdir myscript_result
+cd myscript_result
 mkdir foldername1 foldername2 foldername3
 cd foldername1
 touch 1_text_file.txt 2_text_file.txt 3_text_file.txt 1_json_file.json 2_json_file.json
