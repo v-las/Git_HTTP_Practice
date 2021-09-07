e14e85d43c6fe60d15d40da639f482195b606d8e
v-las <89486551+v-las@users.noreply.github.com>
2021-09-07 17:18:07 +0700

    Update myscript.sh

--- HW_01/myscript/myscript.sh
+++ HW_01/myscript/myscript.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir myscript_result
 cd myscript_result
+mkdir foldername
+cd foldername
 mkdir foldername1 foldername2 foldername3
 cd foldername1
 touch 1_text_file.txt 2_text_file.txt 3_text_file.txt 1_json_file.json 2_json_file.json
